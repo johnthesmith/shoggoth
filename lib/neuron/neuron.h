bc18ec3b3f1e97fe7d6771a7429fe82ace2d802a
John the Thmith <a@b.com>
2023-01-25 00:02:29 +0300

    Neurons

--- lib/neuron/neuron.h
+++ lib/neuron/neuron.h
@@ -3,28 +3,13 @@
 #include <string>
 #include <vector>
 
-# include "../point3.h"
+#include "../point3.h"
+#include "../point3i.h"
 
 using namespace std;
 
 
 class Neuron;
-
-
-
-struct Point3i
-{
-    int x = 0;  /* x */
-    int y = 0;  /* y */
-    int z = 0;  /* z */
-
-    Point3i
-    (
-        int = 0,
-        int = 0,
-        int = 0
-    );
-};
 
 
 
