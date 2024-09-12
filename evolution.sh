8e16b6ff082bd2666c00b0a760e604216f034143
Still Swamp <still@itserv.ru>
2024-09-12 22:19:28 +0300

    Union

--- evolution.sh
+++ evolution.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 ./evolution \
 --config=./net/alpha/evolution.json \
---log=./net/alpha/log/evolution.log \
 --mon=./net/alpha/mon/evolution_main.json
--- src/app/evolution/evolution.cpp
+++ src/app/evolution/evolution.cpp
@@ -5,10 +5,6 @@
 
 
 
-using namespace std;
-
-
-
 int main
 (
     int argc,
